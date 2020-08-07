841476c6002c0a89926aab4b405430a48c3b7aa7
MindfulMinun <importantmails4bc@gmail.com>
2020-08-06 22:57:52 -0500

    IndieWeb (rel-me), footer.njk
    Added rel-me to footer links (IndieWeb)
    Use a Nunjucks include (not a shortcode) for the footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,9 @@
 // @ts-nocheck -- 11ty has no type documentation
 module.exports = function (config) {
-
-    // config.addShortcode('image', await import('./11ty-plugins/image'))
-    // config.addShortcode('footer', await import('./11ty-plugins/footer'))
     config.addShortcode('image', require('./11ty-plugins/image'))
-    config.addShortcode('footer', require('./11ty-plugins/footer'))
     config.addFilter('twemoji', require('./11ty-plugins/twemoji'))
-    
     config.addFilter("addOrigin", path => `https://benjic.xyz${path}`)
-
     config.addPassthroughCopy(`src/static`)
-
     config.addPassthroughCopy({
         'src/static/images/pfp-2020.png': '/favicon.png'
     })
