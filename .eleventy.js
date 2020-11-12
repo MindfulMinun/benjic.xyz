e2e0d16d5cb72687a64ad090c7ffb0f61f8a89be
MindfulMinun <benji@benjic.xyz>
2020-11-12 03:19:27 -0600

    undefined - date filters - Added another container breakpoint at 920px and 1040px

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,42 @@ module.exports = function (config) {
         'src/static/images/pfp-2020.png': '/favicon.png'
     })
 
+    config.addFilter('toDateTime', date => {
+        if (!(date instanceof Date)) {
+            date = new Date(date)
+        }
+
+        return date.toLocaleString('en-US', {
+            weekday: 'short',
+            month: 'short',
+            day: 'numeric',
+            year: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+            hour12: false
+        })
+    })
+
+    config.addFilter('toDate', date => {
+        if (!(date instanceof Date)) {
+            date = new Date(date)
+        }
+
+        return date.toLocaleDateString('en-US', {
+            month: 'short',
+            day: 'numeric',
+            year: 'numeric'
+        })
+    })
+    
+    config.addFilter('toISODateTime', date => {
+        if (!(date instanceof Date)) {
+            date = new Date(date)
+        }
+        return date.toISOString()
+    })
+
     return {
         dir: {
             input: 'src',
